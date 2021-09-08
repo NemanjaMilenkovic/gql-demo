3cfd343baf9cb0d3257f10a99f0b8d478dd16e78
Nemanja Milenkovic <nemanja.milenkovic@gmail.com>
2021-09-08 23:43:39 +0900

    add resolve function

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 const PORT = 4000;
 app.listen(PORT, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+const books = [
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
 	name: 'Book',
@@ -20,7 +28,7 @@ const RootQuery = new GraphQLObjectType({
 				id: { type: GraphQLString },
 			},
 			resolve(parent, args) {
-				// code to get data from the source (db)
+				return _.find(books, { id: args.id });
 			},
 		},
 	},
