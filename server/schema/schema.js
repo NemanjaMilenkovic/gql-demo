aacbfb3ba222c94c625069c1a70f03c43a42e13d
Nemanja Milenkovic <nemanja.milenkovic@gmail.com>
2021-09-10 23:12:30 +0900

    authors root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -82,6 +82,12 @@ const RootQuery = new GraphQLObjectType({
 				return books;
 			},
 		},
+		authors: {
+			type: new GraphQLList(AuthorType),
+			resolve(parent, args) {
+				return authors;
+			},
+		},
 	},
 });
 
