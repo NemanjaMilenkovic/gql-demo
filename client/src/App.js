83a61fc4d4746c655d3d42be8c9240d2b2fa3ae6
Nemanja Milenkovic <nemanja.milenkovic@gmail.com>
2021-09-13 23:05:47 +0900

    add cra

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="main">
+			<h1>Reading list</h1>
+		</div>
+	);
+}
+
+export default App;
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const {
 	GraphQLID,
 	GraphQLInt,
 	GraphQLList,
+	GraphQLNonNull,
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -83,8 +84,8 @@ const Mutation = new GraphQLObjectType({
 		addAuthor: {
 			type: AuthorType,
 			args: {
-				name: { type: GraphQLString },
-				age: { type: GraphQLInt },
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLString) },
 			},
 			resolve(parent, args) {
 				let author = new Author({
@@ -97,9 +98,9 @@ const Mutation = new GraphQLObjectType({
 		addBook: {
 			type: BookType,
 			args: {
-				name: { type: GraphQLString },
-				genre: { type: GraphQLString },
-				authorId: { type: GraphQLID },
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				genre: { type: new GraphQLNonNull(GraphQLString) },
+				authorId: { type: new GraphQLNonNull(GraphQLID) },
 			},
 			resolve(parent, args) {
 				let book = new Book({
