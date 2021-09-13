f296f23c749fee8ab8e9df259aefcdd4d5aa589b
Nemanja Milenkovic <nemanja.milenkovic@gmail.com>
2021-09-14 08:27:17 +0900

    add Apollo Cllient

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import BookList from './components/BookList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql',
+});
 
 function App() {
 	return (
-		<div className="main">
-			<h1>Reading list</h1>
-			<BookList />
-		</div>
+		<ApolloProvider client={client}>
+			<div className="main">
+				<h1>Reading list</h1>
+				<BookList />
+			</div>
+		</ApolloProvider>
 	);
 }
 
