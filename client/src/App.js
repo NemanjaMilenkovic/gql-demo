5b376dd42bc7390becbd1562380c2d5593a217db
Nemanja Milenkovic <nemanja.milenkovic@gmail.com>
2021-09-13 23:10:28 +0900

    add BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import BookList from './components/BookList';
+
 function App() {
 	return (
 		<div className="main">
 			<h1>Reading list</h1>
+			<BookList />
 		</div>
 	);
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,11 @@
+function BookList() {
+	return (
+		<div className="main">
+			<ul id="book-list">
+				<li>Book Name</li>
+			</ul>
+		</div>
+	);
+}
+
+export default BookList;
